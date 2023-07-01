b98d81669103499c19c9d0a39557dda94afd61f9
Anton Evdokimov <a.evdokimov@mts.ai>
2023-07-01 10:29:14 +0300

    DZ1

---
+++ DZ1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    var inputedStr = Console.ReadLine();
+    return Convert.ToInt32(inputedStr);
+}
+
+void Task1()
+{
+    Console.WriteLine("Start task1: ");
+
+    int firstNum = ReadNumber("Input first number > ");
+    int secondNum = ReadNumber("Input second number > ");
+
+    int result = firstNum > secondNum ? firstNum : secondNum;
+
+    Console.WriteLine($"Максимальное число: max={result}");
+    Console.WriteLine("");
+}
+
+Task1();
---
+++ DZ1_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    var inputedStr = Console.ReadLine();
+    return Convert.ToInt32(inputedStr);
+}
+
+void Task2()
+{
+    Console.WriteLine("Start task2: ");
+
+    int firstNum = ReadNumber("Input first number > ");
+    int secondNum = ReadNumber("Input second number > ");
+    int thirdNum = ReadNumber("Input third number > ");
+
+    int result;
+    if (firstNum > secondNum)
+    {
+        if (firstNum > thirdNum)
+        {
+            result = firstNum;
+        }
+        else
+        {
+            result = thirdNum;
+        }
+    }
+    else
+    {
+        if (secondNum > thirdNum)
+        {
+            result = secondNum;
+        }
+        else
+        {
+            result = thirdNum;
+        }
+    }
+
+    Console.WriteLine($"Максимальное число: max={result}");
+    Console.WriteLine("");
+}
+
+Task2();
