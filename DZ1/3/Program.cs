c80f8517edbc35204a6856bd806708ff55a6d58e
Anton Evdokimov <a.evdokimov@mts.ai>
2023-07-01 10:34:25 +0300

    refactor

---
+++ DZ1/3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    var inputedStr = Console.ReadLine();
+    return Convert.ToInt32(inputedStr);
+}
+
+void Task3()
+{
+    Console.WriteLine("Start task3: ");
+    int num = ReadNumber("Input number > ");
+    Console.WriteLine(num % 2 == 0 ? "да" : "нет");
+    Console.WriteLine("");
+}
+
+Task3();
---
+++ DZ1/4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    var inputedStr = Console.ReadLine();
+    return Convert.ToInt32(inputedStr);
+}
+
+void Task4()
+{
+    Console.WriteLine("Start task4: ");
+    int num = ReadNumber("Input first number > ");
+
+    var result = new List<int>();
+    for (var i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine("Add");
+            result.Add(i);
+        }
+    }
+
+    Console.WriteLine(string.Join(", ", result.ToArray()));
+}
+
+Task4();
