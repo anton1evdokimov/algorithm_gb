aaaff28c2105c92f179de4249d5b4e1511a6515c
Anton Evdokimov <a.evdokimov@mts.ai>
2023-07-22 15:46:09 +0300

    DZ6

---
+++ DZ6/1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int count = ReadNumber("Введтите число элеметов: ");
+
+int result = 0;
+
+for (int i = 1; i <= count; i++)
+{
+    if(ReadNumber($"Введтите число номер {i}: ")> 0)
+    {
+        result++;
+    }
+}
+
+Console.WriteLine(result);
---
+++ DZ6/2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int b1 = ReadNumber("Введтите коэффициент b1: ");
+int k1 = ReadNumber("Введтите коэффициент k1: ");
+int b2 = ReadNumber("Введтите коэффициент b2: ");
+int k2 = ReadNumber("Введтите коэффициент k2: ");
+
+// k1* x + b1 = k2 * x + b2;
+
+int deltaB = b2 - b1;
+
+int x = deltaB == 0 ? 0 : (k1 - k2) / (b2 - b1);
+int y = k1 * x + b1;
+
+Console.WriteLine($"x = {x}; y = {y};");
+
+
